0e30a56db4723b3dafc42351b2c47be31b4a69c2
cbb356 <cbb356@gmail.com>
2025-12-03 23:12:05 +0200

    001_01_added

---
+++ 001_Introduction/001_Introduction_HW/01_List/Order.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace List
+{
+    internal class Order
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; } = null!;
+        public DateTime Create { get; set; }
+        public DateTime? Update { get; set; }
+        public string? Description { get; set; }
+
+        public override string ToString()
+        {
+            return $"OrderId: {Id}\nName: {Name}\nCreated: {Create}\nUpdated: {Update}\nDescription: {Description}\n";
+        }
+
+    }
+}
---
+++ 001_Introduction/001_Introduction_HW/01_List/Program.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Security.Cryptography;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace List
+{
+    /*
+    * Розробіть консольну програму з використанням одного списку з типом (List<Order>) 
+    * Замовлення – Order
+    * 1) + Id: Guid
+    * 2) + Name: string
+    * 3) + Create: datetime
+    * 4) + Update: datetime
+    * 5) + Description: string
+    * Заповніть ваш список значеннями (10 значень).
+    * Виведіть на екран значення за індексом > 1, 5, 0, 7
+    * Знайдіть та виведіть індекси > 1, 5 за властивістю Id
+    * Знайдіть та виведіть індекси > 0, 7 за властивістю Name
+    */
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create and fill List of Orders
+            var orders = new List<Order>
+            {
+                new Order
+                {
+                    Id = Guid.Parse("EABD694D-E41B-40F8-BF78-289868E9FAD5"),
+                    Name = "Order01",
+                    Create = new DateTime(2025, 11, 15, 10, 0, 0),
+                    Update = new DateTime(2025, 11, 15, 10, 0, 0),
+                    Description = "Goods"
+                },
+                new Order
+                {
+                    Id = Guid.Parse("7B0301AF-02E5-43B3-B94C-A7E4F5418BE3"),
+                    Name = "Order02",
+                    Create = new DateTime(2025, 11, 18, 14, 30, 0),
+                    Update = new DateTime(2025, 11, 20, 9, 15, 0),
+                    Description = "Software."
+                },
+                new Order
+                {
+                    Id = Guid.Parse("646D8590-B84F-4428-9276-30479E857843"),
+                    Name = "Order03",
+                    Create = new DateTime(2025, 11, 19, 9, 0, 0),
+                    Update = null,
+                    Description = "Materials"
+                },
+                new Order
+                {
+                    Id = Guid.Parse("E1A98A97-EF71-4CC7-9EC6-2CA7E2D7D31F"),
+                    Name = "Order04",
+                    Create = new DateTime(2025, 11, 21, 11, 45, 0),
+                    Update = new DateTime(2025, 11, 25, 16, 0, 0),
+                    Description = "Monitors"
+                },
+                new Order
+                {
+                    Id = Guid.Parse("6D403881-F8CE-4D74-9BA5-2D17BC630058"),
+                    Name = "Order05",
+                    Create = new DateTime(2025, 11, 22, 16, 20, 0),
+                    Update = null,
+                    Description = null
+                },
+                new Order
+                {
+                    Id = Guid.Parse("8999AD9D-5CA8-47E1-A14D-16D397AE0BF8"),
+                    Name = "Order06",
+                    Create = new DateTime(2025, 11, 25, 8, 0, 0),
+                    Update = new DateTime(2025, 11, 25, 13, 0, 0),
+                    Description = "Custom tools"
+                },
+                new Order
+                {
+                    Id = Guid.Parse("E65CDF84-1D15-4B1C-BCA8-7507AEA951D2"),
+                    Name = "Order07",
+                    Create = new DateTime(2025, 11, 26, 17, 10, 0),
+                    Update = new DateTime(2025, 12, 1, 11, 0, 0),
+                    Description = "Service"
+                },
+                new Order
+                {
+                    Id = Guid.Parse("174124FA-E340-4763-8DD3-E721ACE0420D"),
+                    Name = "Order08",
+                    Create = new DateTime(2025, 11, 28, 9, 30, 0),
+                    Update = null,
+                    Description = "Tools"
+                },
+                new Order
+                {
+                    Id = Guid.Parse("A8FED3C6-F018-4869-A3D7-4BBFBDB4E186"),
+                    Name = "Order09",
+                    Create = new DateTime(2025, 12, 2, 13, 0, 0),
+                    Update = new DateTime(2025, 12, 3, 10, 0, 0),
+                    Description = "Software"
+                },
+                new Order
+                {
+                    Id = Guid.Parse("51ABE043-5FED-4C0D-9F27-ECA5D8A48A6B"),
+                    Name = "Order10",
+                    Create = new DateTime(2025, 12, 3, 15, 0, 0),
+                    Update = null,
+                    Description = null
+                }
+            };
+
+            //Show orders by index in the list
+            ShowByIndex(orders, 1);
+            ShowByIndex(orders, 5);
+            ShowByIndex(orders, 0);
+            ShowByIndex(orders, 7);
+            ShowByIndex(orders, 12);
+
+            //Show orders by id
+            ShowIndexById(orders, "7B0301AF-02E5-43B3-B94C-A7E4F5418BE3");
+            ShowIndexById(orders, "8999AD9D-5CA8-47E1-A14D-16D397AE0BF8");
+            ShowIndexById(orders, "00000000-0000-0000-0000-000000000000");
+            ShowIndexById(orders, "NonGuid");
+
+            //Show orders by name
+            ShowIndexByName(orders, "Order01");
+            ShowIndexByName(orders, "Order08");
+            ShowIndexByName(orders, "NonExistentOrder");
+
+            // Delay.
+            Console.ReadKey();
+        }
+
+        public static void ShowByIndex(List<Order> list, int index)
+        {
+            try
+            {
+                Order order = list[index];
+                if (order != null)
+                {
+                    Console.WriteLine($"The order with index {index}:\n{order.ToString()}");
+                }
+                else
+                {
+                    Console.WriteLine($"The order with index {index} was not found\n");
+                }
+            }
+            catch (Exception ex) 
+            {
+                Console.WriteLine($"Error finding order with index{index}: {ex.Message}\n");
+            }
+        }
+
+        public static void ShowIndexById(List<Order> list, string id)
+        {
+            try
+            {
+                int index = list.FindIndex(x => x.Id == Guid.Parse(id));
+                if (index != -1)
+                {
+                    Console.WriteLine($"The index of order with Id {id} is: {index}\n");
+                }
+                else
+                {
+                    Console.WriteLine($"The order with Id {id} was not found, index is -1\n");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error finding index for Id {id}: {ex.Message}\n");
+            }
+        }
+        public static void ShowIndexByName(List<Order> list, string name)
+        {
+            try
+            {
+                int index = list.FindIndex(x => x.Name == name);
+                if (index != -1)
+                {
+                    Console.WriteLine($"The index of order with Name {name} is: {index}\n");
+                }
+                else
+                {
+                    Console.WriteLine($"The order with Name {name} was not found, index is -1\n");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error finding index for Name {name}: {ex.Message}\n");
+            }
+        }
+    }
+}
