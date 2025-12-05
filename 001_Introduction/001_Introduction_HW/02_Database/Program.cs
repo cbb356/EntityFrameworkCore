82e0d7fa1b79dacc02bc5af461c736e26a957601
cbb356 <cbb356@gmail.com>
2025-12-05 23:09:06 +0200

    001_02_refactored

--- 001_Introduction/001_Introduction_HW/02_Database/Program.cs
+++ 001_Introduction/001_Introduction_HW/02_Database/Program.cs
@@ -8,6 +8,8 @@
  * з Product/User/Order (ваш варінт) контексту за ім’ям
  */
 
+using Microsoft.EntityFrameworkCore;
+
 namespace Database
 {
     internal class Program
@@ -16,99 +18,95 @@ namespace Database
         {
             using (var context = new MyDatabaseContext())
             {
-                if (!context.Orders.Any())
-                {
-                    Console.WriteLine("Seeding database with initial data...");
-                    context.Orders.AddRange(
-                        new Order
-                        {
-                            Id = Guid.Parse("EABD694D-E41B-40F8-BF78-289868E9FAD5"),
-                            Name = "Order01",
-                            Create = new DateTime(2025, 11, 15, 10, 0, 0),
-                            Update = new DateTime(2025, 11, 15, 10, 0, 0),
-                            Description = "Goods"
-                        },
-                        new Order
-                        {
-                            Id = Guid.Parse("7B0301AF-02E5-43B3-B94C-A7E4F5418BE3"),
-                            Name = "Order02",
-                            Create = new DateTime(2025, 11, 18, 14, 30, 0),
-                            Update = new DateTime(2025, 11, 20, 9, 15, 0),
-                            Description = "Software."
-                        },
-                        new Order
-                        {
-                            Id = Guid.Parse("646D8590-B84F-4428-9276-30479E857843"),
-                            Name = "Order03",
-                            Create = new DateTime(2025, 11, 19, 9, 0, 0),
-                            Update = null,
-                            Description = "Materials"
-                        },
-                        new Order
-                        {
-                            Id = Guid.Parse("E1A98A97-EF71-4CC7-9EC6-2CA7E2D7D31F"),
-                            Name = "Order04",
-                            Create = new DateTime(2025, 11, 21, 11, 45, 0),
-                            Update = new DateTime(2025, 11, 25, 16, 0, 0),
-                            Description = "Monitors"
-                        },
-                        new Order
-                        {
-                            Id = Guid.Parse("6D403881-F8CE-4D74-9BA5-2D17BC630058"),
-                            Name = "Order05",
-                            Create = new DateTime(2025, 11, 22, 16, 20, 0),
-                            Update = null,
-                            Description = null
-                        },
-                        new Order
-                        {
-                            Id = Guid.Parse("8999AD9D-5CA8-47E1-A14D-16D397AE0BF8"),
-                            Name = "Order06",
-                            Create = new DateTime(2025, 11, 25, 8, 0, 0),
-                            Update = new DateTime(2025, 11, 25, 13, 0, 0),
-                            Description = "Custom tools"
-                        },
-                        new Order
-                        {
-                            Id = Guid.Parse("E65CDF84-1D15-4B1C-BCA8-7507AEA951D2"),
-                            Name = "Order07",
-                            Create = new DateTime(2025, 11, 26, 17, 10, 0),
-                            Update = new DateTime(2025, 12, 1, 11, 0, 0),
-                            Description = "Service"
-                        },
-                        new Order
-                        {
-                            Id = Guid.Parse("174124FA-E340-4763-8DD3-E721ACE0420D"),
-                            Name = "Order08",
-                            Create = new DateTime(2025, 11, 28, 9, 30, 0),
-                            Update = null,
-                            Description = "Tools"
-                        },
-                        new Order
-                        {
-                            Id = Guid.Parse("A8FED3C6-F018-4869-A3D7-4BBFBDB4E186"),
-                            Name = "Order09",
-                            Create = new DateTime(2025, 12, 2, 13, 0, 0),
-                            Update = new DateTime(2025, 12, 3, 10, 0, 0),
-                            Description = "Software"
-                        },
-                        new Order
-                        {
-                            Id = Guid.Parse("51ABE043-5FED-4C0D-9F27-ECA5D8A48A6B"),
-                            Name = "Order10",
-                            Create = new DateTime(2025, 12, 3, 15, 0, 0),
-                            Update = null,
-                            Description = null
-                        }
-                    );
-                    context.SaveChanges();
-                    Console.WriteLine("Data saved successfully.\n");
-                }
-                else
-                {
-                    Console.WriteLine("Database already contains data. Skipping insertion.\n");
-                }
-                 
+                // Delete old data
+                context.Orders.ExecuteDelete();
+                // Add data
+                Console.WriteLine("Seeding database with initial data...");
+                context.Orders.AddRange(
+                    new Order
+                    {
+                        Id = Guid.Parse("EABD694D-E41B-40F8-BF78-289868E9FAD5"),
+                        Name = "Order01",
+                        Create = new DateTime(2025, 11, 15, 10, 0, 0),
+                        Update = new DateTime(2025, 11, 15, 10, 0, 0),
+                        Description = "Goods"
+                    },
+                    new Order
+                    {
+                        Id = Guid.Parse("7B0301AF-02E5-43B3-B94C-A7E4F5418BE3"),
+                        Name = "Order02",
+                        Create = new DateTime(2025, 11, 18, 14, 30, 0),
+                        Update = new DateTime(2025, 11, 20, 9, 15, 0),
+                        Description = "Software."
+                    },
+                    new Order
+                    {
+                        Id = Guid.Parse("646D8590-B84F-4428-9276-30479E857843"),
+                        Name = "Order03",
+                        Create = new DateTime(2025, 11, 19, 9, 0, 0),
+                        Update = null,
+                        Description = "Materials"
+                    },
+                    new Order
+                    {
+                        Id = Guid.Parse("E1A98A97-EF71-4CC7-9EC6-2CA7E2D7D31F"),
+                        Name = "Order04",
+                        Create = new DateTime(2025, 11, 21, 11, 45, 0),
+                        Update = new DateTime(2025, 11, 25, 16, 0, 0),
+                        Description = "Monitors"
+                    },
+                    new Order
+                    {
+                        Id = Guid.Parse("6D403881-F8CE-4D74-9BA5-2D17BC630058"),
+                        Name = "Order05",
+                        Create = new DateTime(2025, 11, 22, 16, 20, 0),
+                        Update = null,
+                        Description = null
+                    },
+                    new Order
+                    {
+                        Id = Guid.Parse("8999AD9D-5CA8-47E1-A14D-16D397AE0BF8"),
+                        Name = "Order06",
+                        Create = new DateTime(2025, 11, 25, 8, 0, 0),
+                        Update = new DateTime(2025, 11, 25, 13, 0, 0),
+                        Description = "Custom tools"
+                    },
+                    new Order
+                    {
+                        Id = Guid.Parse("E65CDF84-1D15-4B1C-BCA8-7507AEA951D2"),
+                        Name = "Order07",
+                        Create = new DateTime(2025, 11, 26, 17, 10, 0),
+                        Update = new DateTime(2025, 12, 1, 11, 0, 0),
+                        Description = "Service"
+                    },
+                    new Order
+                    {
+                        Id = Guid.Parse("174124FA-E340-4763-8DD3-E721ACE0420D"),
+                        Name = "Order08",
+                        Create = new DateTime(2025, 11, 28, 9, 30, 0),
+                        Update = null,
+                        Description = "Tools"
+                    },
+                    new Order
+                    {
+                        Id = Guid.Parse("A8FED3C6-F018-4869-A3D7-4BBFBDB4E186"),
+                        Name = "Order09",
+                        Create = new DateTime(2025, 12, 2, 13, 0, 0),
+                        Update = new DateTime(2025, 12, 3, 10, 0, 0),
+                        Description = "Software"
+                    },
+                    new Order
+                    {
+                        Id = Guid.Parse("51ABE043-5FED-4C0D-9F27-ECA5D8A48A6B"),
+                        Name = "Order10",
+                        Create = new DateTime(2025, 12, 3, 15, 0, 0),
+                        Update = null,
+                        Description = null
+                    }
+                );
+                context.SaveChanges();
+                Console.WriteLine("Data saved successfully.\n");
+
                 //Show orders by name
                 ShowByName(context, "Order02");
                 ShowByName(context, "Order06");
@@ -116,7 +114,6 @@ namespace Database
                 ShowByName(context, "Order08");
                 ShowByName(context, "NonExistentOrder");
             }
-
             // Delay.
             Console.ReadKey();
         }
